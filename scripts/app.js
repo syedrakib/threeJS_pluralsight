f08d3db596b8c8141c6c556508d1f15ffa95bb8f
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-11-06 19:46:02 +0600

    Rotate the cube

--- scripts/app.js
+++ scripts/app.js
@@ -24,6 +24,8 @@ var sceneObj = (function(){
 
     function render(){
         renderer.render(scene, camera);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
         requestAnimationFrame(render);
     }
 
