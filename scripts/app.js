6ff5877cfd10f45bf3f62b01e6ec216621c70baf
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-11-06 19:39:31 +0600

    Draw a simple red cube

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+(function(){
+
+    var scene, camera, renderer, cube;
+
+    function initScene(){
+        scene = new THREE.Scene();
+
+        camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight , 1, 1000);
+        camera.position.z = 100;
+
+        renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById("webgl-container").appendChild(renderer.domElement);
+
+        cube = new THREE.Mesh(
+            new THREE.BoxGeometry(25,25,25),
+            new THREE.MeshBasicMaterial({color: 0xff0000})
+        );
+        scene.add(cube);
+
+        render();
+    }
+
+    function render(){
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+
+    window.onLoad = initScene();
+
+})();
